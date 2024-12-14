e8ddd07fa38ecb951bb6ddee336f3ddd521a219a
xingxiaolin <xing.xiaolin@foxmail.com>
2024-12-14 08:56:32 +0800

    [docs]数据库名变更为my_grafana，完善文档中

--- volumes/mysql/init.d/grafana.sql
+++ volumes/mysql/init.d/grafana.sql
@@ -1,9 +1,9 @@
 # 创建数据库grafana
-CREATE DATABASE grafana DEFAULT CHARACTER SET utf8mb4;
+CREATE DATABASE my_grafana DEFAULT CHARACTER SET utf8mb4;
 
 # 创建用户
 CREATE USER 'admin_grafana'@'%' IDENTIFIED BY 'password_grafana';
 
 # 权限授予（使用更安全的方式管理密码）
-GRANT ALL PRIVILEGES ON grafana.* TO 'admin_grafana'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON my_grafana.* TO 'admin_grafana'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
